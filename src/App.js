f2b49b00a686f24fc7a941a8099f5582c93f0ad0
Doug Franklin <doug@dougfranklin.com>
2023-05-01 11:28:41 -0400

    Extremely Basic Grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// https://www.ag-grid.com/react-data-grid/cell-editing/
 import './App.css';
 
+import { AgGridReact } from 'ag-grid-react';
+
 function App() {
+
+  const rowData = [
+    { make: 'Ford', model: 'Focus', price: '40000'},
+    { make: 'Toyota', model: 'Celica', price: '45000'},
+    { make: 'BMW', model: '4 Series', price: '40000'}
+  ];
+
+  const columnDefs = [
+
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AgGridReact 
+        rowData={rowData}
+        columnDefs={columnDefs}
+      />
     </div>
   );
 }
