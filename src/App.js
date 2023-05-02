d1fc99a4c389d3510af2245ca8112f7f7e86ae18
Doug Franklin <doug@dougfranklin.com>
2023-05-02 06:24:36 -0400

    react-bootstrap Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // https://www.ag-grid.com/react-data-grid/cell-editing/
 import './App.css';
 
-import { AgGridReact } from 'ag-grid-react';
-
 import { useState, useEffect } from 'react';
 
+import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
